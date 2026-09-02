3503379817e1b1671b582fa533dc1ae19723b39f
DanielDiMarco007 <dimarcodaniel0306@gmail.com>
2026-09-02 14:39:28 -0500

    Fase 1: arquitectura del proyecto #2

--- app/test_app.py
+++ app/test_app.py
@@ -2,7 +2,7 @@ from app import app
 
 
 def test_health_endpoint():
-    """Prueba básica: el endpoint /health responde 200 sin necesidad de BD real."""
+    """Prueba básica: el endpoint /health responde 200 ."""
     client = app.test_client()
     response = client.get("/health")
     assert response.status_code == 200
